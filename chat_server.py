3e11878f39b5a5bfcdb91b93ac00203895c4235f
minseokey <lms990427@kookmin.ac.kr>
2023-12-27 11:21:10 +0900

    feat: add execute func

--- chat_server.py
+++ chat_server.py
@@ -13,3 +13,13 @@ async def index(request):
 async def chat(request):
     f = open('./templates/chat.html')
     return web.Response(text=f.read(), content_type="text/html")
+
+
+async def init():
+    app = web.Application()
+    app.add_routes(routes)
+    app["sockets"] = dict()
+    return app
+
+if __name__ == "__main__":
+    web.run_app(init())
