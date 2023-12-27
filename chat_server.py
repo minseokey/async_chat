4ac0da50975931661c0bc92d2da21770880d5c94
minseokey <lms990427@kookmin.ac.kr>
2023-12-27 11:27:58 +0900

    feat: aiohttp websoket server

--- chat_server.py
+++ chat_server.py
@@ -14,6 +14,26 @@ async def chat(request):
     f = open('./templates/chat.html')
     return web.Response(text=f.read(), content_type="text/html")
 
+@routes.get("/ws")
+async def websoket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    # 1. 연결단계
+    user = await ws.receive_str()
+    if user not in request.app['sockets']:
+        request.app['sockets'][user] = ws
+
+    # 2. 채팅단계
+    async for msg in ws:
+        if msg.type == web.WSMsgType.TEXT:
+            msg_json = msg.json()
+            for socket in request.app['sockets'].keys():
+                await request.app['sockets'][socket].send_json(msg_json)
+
+    # 3. 종료단계
+    del request.app['sockets'][user]
+    return ws
 
 async def init():
     app = web.Application()
